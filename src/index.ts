8bd4d9f7b7632a90642fc7207fde3cfc020a1f4f
andrazvrecko <andraz.vrecko@student.um.si>
2022-04-15 14:25:16 +0200

    moved snap implementations to snap project

--- src/index.ts
+++ src/index.ts
@@ -5,5 +5,4 @@ const schema = require('../plugin.schema.json')
 export { schema }
 export { VCManager } from './agent/vc-manager'
 export { MemoryVCStore } from './vc-store/vc-store'
-export { SnapDIDStore, SnapKeyStore, SnapPrivateKeyStore, SnapVCStore } from './snap-store/snapDataStore'
 export * from './types/IVCManager'
